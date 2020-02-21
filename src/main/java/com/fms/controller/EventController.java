fd18ebf4d82f4a3b56ddea2d90d53237fde834ea
S <724179@cognizant.com>
2020-02-21 13:51:25 +0530

    Initial commit

---
+++ src/main/java/com/fms/controller/EventController.java
@@ -0,0 +1,29 @@
+package com.fms.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fms.model.EventDetailResponse;
+import com.fms.service.EventSummaryService;
+@RestController
+@CrossOrigin(origins = "*")
+public class EventController {
+	@Autowired
+	private EventSummaryService eventSummaryService;
+	@GetMapping("/getEventDetails/{role}")
+	public List<EventDetailResponse> getAllEvents(@PathVariable String role,@RequestHeader String id,HttpServletRequest request){
+	
+		System.out.println("iD"+id);
+		
+		return eventSummaryService.getEventDetails(role,id);
+		
+	}
+}
